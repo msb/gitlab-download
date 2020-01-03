252435fd4b13009b9f3282d705e5028e64e77238
Mike Bamford <mike@msb.me.uk>
2020-01-03 20:48:50 +0000

    - test if the local project file exists to avoid errors - check that project has a repo to download

--- gitlabdownload.py
+++ gitlabdownload.py
@@ -18,9 +18,6 @@ def main():
     This script iterates over a group of gitlab projects and generates a script that either clones
     the projects if they don't exist or pulls the project, if it does. The project group structure
     is preserved.
-
-    TODO test if the local project file exists to avoid errors
-    TODO test for "You are not allowed to download code from this project."
     """
     gl = gitlab.Gitlab(
         os.environ.get('GITLAB_URL', GITLAB_URL), private_token=os.environ['GITLAB_PRIVATE_TOKEN']
@@ -30,15 +27,20 @@ def main():
 
     with open_fs('mem://') as output_path:
         output_path.writetext(script_name, f'# generated: {datetime.datetime.now()}\n')
-
+        output_path.appendtext(
+            script_name,
+            'clone_or_pull() {\n'
+            'if [ -e "$1" ]; then\necho $1\ngit -C $1 pull\nelse\ngit clone $2 $1\nfi\n}\n'
+        )
         for group in gl.groups.list(all_available=True, all=True):
             if group.full_path.startswith(os.environ['GITLAB_GROUP_PATH']):
                 LOGGER.info(group.name)
                 for project in group.projects.list(all=True):
-                    output_path.appendtext(script_name, (
-                        f'git -C {project.path_with_namespace} pull || '
-                        f'git clone {project.ssh_url_to_repo} {project.path_with_namespace}\n'
-                    ))
+                    if project.repository_access_level != 'disabled':
+                        output_path.appendtext(script_name, (
+                            f'clone_or_pull '
+                            f'{project.path_with_namespace} {project.ssh_url_to_repo}\n'
+                        ))
 
         copy_file(output_path, script_name, os.environ['OUTPUT_PATH'], script_name)
 
